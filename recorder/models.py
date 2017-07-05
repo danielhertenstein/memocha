ab6046e89f4497272074ffc9d316be2110d47112
Daniel Hertenstein <daniel.hertenstein@gmail.com>
2017-07-05 13:46:47 -0400

    Adds email fields to human models

--- recorder/models.py
+++ recorder/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 
 class Doctor(models.Model):
+    # TODO: Probably move these details and the ones in Person to a metaclass
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    email = models.EmailField()
 
     def __str__(self):
         return "{0} {1}".format(self.first_name, self.last_name)
@@ -11,6 +13,7 @@ class Doctor(models.Model):
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    email = models.EmailField()
     # The doctors field may not be necessary? Are doctors users who will check
     # on Persons? Is there a way to tie the user to a model instance?
     doctors = models.ManyToManyField(Doctor)
