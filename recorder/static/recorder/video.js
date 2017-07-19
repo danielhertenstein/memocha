8f0f6553917ca45e0a91676edf6fedacf4112a74
Daniel Hertenstein <daniel.hertenstein@gmail.com>
2017-07-19 13:41:52 -0400

    Adds more to the video recording javascript.

--- recorder/static/recorder/video.js
+++ recorder/static/recorder/video.js
@@ -1,10 +1,14 @@
 // Code adapted from: https://github.com/webrtc/samples/blob/gh-pages/src/content/getusermedia/record/js/main.js
 
+var recordedBlobs;
+var mediaRecorder;
+
 var video = document.querySelector('video');
 
 var record_button = document.querySelector('button#record');
 var upload_button = document.querySelector('button#upload');
 record_button.onclick = toggleRecording;
+upload_button.onclick = upload;
 
 var constraints = {
     audio: true,
@@ -37,4 +41,40 @@ function toggleRecording() {
         record_button.textContent = 'Start Recording';
         upload_button.disabled = false;
     }
+}
+
+function startRecording() {
+    recordedBlobs = [];
+    try {
+        mediaRecorder = new MediaRecorder(window.stream);
+    } catch(e) {
+        console.error('Exception while creating MediaRecorder: ' + e);
+        return;
+    }
+    console.log('Created MediaRecorder', mediaRecorder);
+    recordButton.textContent = 'Stop Recording';
+    upload_button.disabled = true;
+    mediaRecorder.onstop = handleStop;
+    mediaRecorder.ondataavailable = handleDataAvailable;
+    mediaRecorder.start(10); // 10ms blobs of data
+    console.log('MediaRecorder started', mediaRecorder);
+}
+
+function stopRecording() {
+    mediaRecorder.stop();
+    console.log('Recorded Blobs: ', recordedBlobs);
+}
+
+function handleDataAvailable(event) {
+    if (event.data && event.data.size > 0) {
+        recordedBlobs.push(event.data);
+    }
+}
+
+function handleStop(event) {
+    console.log('Recorder stopped: ', event);
+}
+
+function upload() {
+    // Fill in.
 }
